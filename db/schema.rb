a950ac8def439c7bae8c14821e5e7005e9f7db4a
Aadil Ahmed Adam <aadil.adam@SLP160130-Aadil.local>
2020-09-08 01:41:34 +0530

    FEAT: ADDED MIGRATION FOR APPOINTMENT TABLE

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_191615) do
+ActiveRecord::Schema.define(version: 2020_09_07_144701) do
 
   create_table "appointments", force: :cascade do |t|
-    t.date "date"
-    t.time "time"
+    t.datetime "appointment_date"
     t.integer "doctor_id"
     t.integer "patient_id"
     t.datetime "created_at", precision: 6, null: false
