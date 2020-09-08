b68c7677d9199db967e9b790c51fac965f6079d2
Aadil Ahmed Adam <aadil.adam@SLP160130-Aadil.local>
2020-09-09 01:02:46 +0530

    FEAT: ADDITION OF USER TABLE AND MIGRATION FILES

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_203244) do
+ActiveRecord::Schema.define(version: 2020_09_08_183545) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_date"
@@ -47,10 +47,15 @@ ActiveRecord::Schema.define(version: 2020_09_07_203244) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "auth_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
